aff345495ebeb05f7d7998fc1e8ad01936364e7d
Mani Goyal <mani73842@gmail.com>
2019-12-09 10:49:31 +0530

    Updates

---
+++ precache-manifest.1a489ee229ad968dc7f75aa18d1870cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c36ae0886ea87630bebe20956b749ab",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "0374bc65fb130755a503",
+    "url": "/test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "70c78b0e816a18c8657a",
+    "url": "/test/static/js/2.1f07b838.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test/static/js/2.1f07b838.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0374bc65fb130755a503",
+    "url": "/test/static/js/main.4bda07ea.chunk.js"
+  },
+  {
+    "revision": "6faa450e8e057dc3865c",
+    "url": "/test/static/js/runtime-main.d5f6199e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
